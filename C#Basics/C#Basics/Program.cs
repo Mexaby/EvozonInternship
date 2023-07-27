ce72f22bbf310ce7d5db600d455d76342614ac12
csaba.gyarmathy <csaba.gyarmathy@evozon.com>
2023-07-27 10:27:55 +0300

    C#Basics

---
+++ C#Basics/C#Basics/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Assignemnt 1:
+            //1
+            Console.WriteLine("Hello");
+            Console.WriteLine("Csabi");
+
+            //2
+            Console.WriteLine(3 + 2);
+
+            //3
+            Console.WriteLine((float)5 / 3);
+
+            //4
+            Console.WriteLine(-5 + 8 * 6);
+            Console.WriteLine((55 + 9) % 9);
+            Console.WriteLine(20 + -3 * 5 / 8);
+            Console.WriteLine(+15 / 3 * 2 - 8 % 3);
+
+
+            Console.ReadLine();
+        }
+    }
+}
