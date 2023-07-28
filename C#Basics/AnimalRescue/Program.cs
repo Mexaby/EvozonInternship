fd947570b8695c6ac7f107e72dc2e008ca290139
csaba.gyarmathy <csaba.gyarmathy@evozon.com>
2023-07-28 09:49:12 +0300

    Added AnimalRescue project

--- C#Basics/AnimalRescue/Program.cs
+++ C#Basics/AnimalRescue/Program.cs
@@ -4,15 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace C_Basics.AnimalRescue
+namespace AnimalRescue
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Gay");
-
-            Console.ReadKey();
         }
     }
 }
